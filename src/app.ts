7e85951314d7f1609ee0087fe69516d71f832336
sakthi s <velsakthis5301@gmail.com>
2021-12-29 18:25:37 +0530

    first commit

---
+++ src/app.ts
@@ -0,0 +1,17 @@
+import * as express from "express"
+import * as cors from 'cors'
+import { createConnection } from "typeorm"
+
+createConnection().then(db => {
+    const app = express()
+
+    app.use(cors({
+        origin: ['http://localhot:3000', 'http://localhost:8000', 'http://localhost:4200']
+    }))
+    
+    app.use(express.json())
+    
+    console.log('Listening to port: 8000')
+    app.listen(8000)
+})
+
